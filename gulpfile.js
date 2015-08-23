d5bc6cc2b6959983584be9778d06e6d09ad47e9a
Anderson Tao <andeetao@gmail.com>
2015-08-23 15:11:41 -0700

    added gulp minify css task

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ var gulp = require('gulp'),
 	concat = require('gulp-concat'),
 	rename = require('gulp-rename'),
 	browserSync = require('browser-sync').create(),
-	uglify = require('gulp-uglify')
+	uglify = require('gulp-uglify'),
+	minifyCss = require('gulp-minify-css'),
 	jasmine = require('gulp-jasmine');
 
 // Jasmine Testing
@@ -34,7 +35,13 @@ gulp.task('server', function(){
 	});
 	// gulp.watch("js/*.js", ['js-watch']);
 	gulp.watch("js/lib/*.js", ['js-watch']);
+	gulp.watch("styles/*.css", ['minify-css']);
 });
 
-
+//minify css
+gulp.task('minify-css', function() {
+  return gulp.src('styles/*.css')
+    .pipe(minifyCss())
+    .pipe(gulp.dest('dist/styles'));
+});
 
